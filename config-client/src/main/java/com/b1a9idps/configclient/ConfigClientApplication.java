835b9cb465e4924cd4f192e4b2d8840d29d7f7f2
Ryosuke <hello.ryosuke.u@gmail.com>
2022-02-13 16:45:37 +0900

    Add Spring Cloud Client

--- config-client/src/main/java/com/b1a9idps/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/b1a9idps/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.b1a9idps.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = {"com.b1a9idps.configclient.properties"})
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
